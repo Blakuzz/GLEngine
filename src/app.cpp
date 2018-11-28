2e5db5fae28ca55c4986d434b34b39d2852e3733
Damiano Bianda <damianobianda11@gmail.com>
2018-11-28 14:17:37 +0100

    Shaders support:
    - added basic shaders
    - added vertex shader compilation

--- src/app.cpp
+++ src/app.cpp
@@ -1,17 +1,70 @@
 #include "Engine.h"
 
+#include <string>
+#include <fstream>
+#include <iostream>
+
 void processUserInput(Engine& engine) {
 	if (engine.eventHappened(GLFW_KEY_ESCAPE, GLFW_PRESS)) {
 		engine.closeWindow();
 	}
 }
 
+std::string readFile(std::string path) {
+	std::ifstream ifs(path);
+	std::string content((std::istreambuf_iterator<char>(ifs)),
+		(std::istreambuf_iterator<char>()));
+	return content;
+}
+
 int main(int argc, char** argv) {
 	
+	std::string vertex_file = readFile("..\\engine\\glsl\\vertex.glsl");
+	std::string fragment_file = readFile("..\\engine\\glsl\\fragment.glsl");
+	
+	if (vertex_file.size() == 0) {
+		std::cerr << "Vertex shader file is empty or path is wrong" << std::endl;
+		return 1;
+	}
+
+	if (fragment_file.size() == 0) {
+		std::cerr << "Fragment shader file is empty or path is wrong" << std::endl;
+		return 1;
+	}
+	
+
 	Engine engine = Engine(1024, 768);
 	if (!engine.init()) {
 		return 1;
 	}
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	const char *vertex_file_c_str = vertex_file.c_str();
+	glShaderSource(vertexShader, 1, &vertex_file_c_str, NULL);
+	glCompileShader(vertexShader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cerr << "Error during shader compilation\n" << infoLog << std::endl;
+		return 1;
+	}
+
+
 
 	while (!engine.windowsWasClosed()) {
 		engine.clearColor(0.2f, 0.3f, 0.3f, 1.0f);
